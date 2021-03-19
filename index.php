63b8a0c75d4f8e62d2d5459ba2624bcfd17ee855
priyankaaurade1 <70333636+priyankaaurade1@users.noreply.github.com>
2021-03-20 05:14:26 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Card</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+    <div class="container">
+    <!-- product  1 -->      
+            <div class="card">
+                <div class="content-1">
+                    <div class="main-image">
+                        <img id="product1" src="images/sh22.png" alt="">   
+                    </div>
+                </div>
+                <div class="content-2">
+                    <div class="price">      
+                        <span>$55</span>
+                        <div class="space"></div>
+                        <button class="btn-change">Buy Now</button>                      
+                    </div>
+                    <div class="branding">
+                        <h4>fashion has been creating well designed collections</h4>
+                    </div>
+                </div> 
+            </div>             
+    <!-- product 2 -->
+            <div class="card">
+                <div class="content-1">           
+                    <div class="main-image">
+                        <img src="images/sh11.png" alt="">                    
+                    </div>
+                </div>
+                <div class="content-2">
+                    <div class="price">      
+                        <span>$55</span>
+                        <div class="space"></div>
+                        <button class="btn-change">Buy Now</button>                   
+                    </div>
+                    <div class="branding">
+                        <h4>fashion has been creating well designed collections</h4>
+                    </div>
+                </div> 
+            </div>
+        
+    <!-- product 3 -->
+            <div class="card">
+                <div class="content-1">
+                    <div class="main-image">
+                        <img src="images/sh33.png" alt="">         
+                    </div>
+                </div>
+                <div class="content-2">
+                    <div class="price">      
+                        <span>$55</span>
+                        <div class="space"></div>
+                        <button class="btn-change">Buy Now</button>                    
+                    </div>
+                    <div class="branding">
+                        <h4>fashion has been creating well designed collections</h4>
+                    </div>
+                </div> 
+            </div>
+        <!-- product end -->
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+            crossorigin="anonymous"></script>
+    <script src="1.js"></script>
+</body>
+</html>
+?>
